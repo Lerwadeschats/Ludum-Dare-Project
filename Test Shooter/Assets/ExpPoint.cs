dc5470173ac47858abd51dbd4dce77f98b2e37f4
Sacha (Pc) <S.soulslasher@gmail.com>
2022-10-02 16:31:32 +0200

    EXP VIENS VERS MOI

--- Test Shooter/Assets/ExpPoint.cs
+++ Test Shooter/Assets/ExpPoint.cs
@@ -7,22 +7,27 @@ public class ExpPoint : MonoBehaviour
     public float expValue;
     GameObject player;
 
+    public Rigidbody2D rb;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(player.transform.position, transform.position) <= 5)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, .1f);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("hurtbox"))
         {
-            Debug.Log("Oui");
             player.GetComponent<PlayerController>().xpPlayer += expValue;
             Destroy(gameObject);
         }
