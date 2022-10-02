7703c194e8a0ee753103bc7eb4c2ab621b327100
Emile Birraux <Emile.birraux@edu.devinci.fr>
2022-10-02 12:04:47 +0200

    DASH
    DASH

--- Test Shooter/Assets/Script/PlayerController.cs
+++ Test Shooter/Assets/Script/PlayerController.cs
@@ -13,6 +13,13 @@ public class PlayerController : MonoBehaviour
     Vector2 movement;
     Vector2 mousePos;
 
+    private bool isDashing = false;
+    private bool enabledDash = false;
+    private bool canDash = true;
+    private float dashPower = 50f;
+    private float dashCooldown = 1;
+    private float dashTime = 0.1f;
+
     void Start()
     {
         rbPlayer = gameObject.GetComponent<Rigidbody2D>();
@@ -25,14 +32,34 @@ public class PlayerController : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDash == true)
+        {
+            StartCoroutine(Dashing());
+        }
+
     }
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         rbPlayer.MovePosition(rbPlayer.position + movement * speedPlayer * Time.fixedDeltaTime);
 
         Vector2 lookDir = mousePos - rbPlayer.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rbPlayer.rotation = angle;
+    }
+    public  IEnumerator Dashing()
+    {
+        Debug.Log("HEY");
+        isDashing = true;
+        canDash = false;
+        rbPlayer.velocity = new Vector2(movement.x * dashPower, movement.y * dashPower);
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
