041435fc9313e42e5e51667b0efbf4a792818c94
Sacha (Pc) <S.soulslasher@gmail.com>
2022-10-03 17:28:28 +0200

    MENU

---
+++ Test Shooter/Assets/Script/SceneToScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneToScene : MonoBehaviour
+{
+    public Scene ye;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void yeeeet()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
